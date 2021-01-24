90a4fe214d4002ce9ce480d6ff47663da71c7aac
yonathan734 <77904224+yonathan734@users.noreply.github.com>
2021-01-24 21:09:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ limitawal = '20'
 cr = '*BOT INI SUDAH TERVERIFIKASI*'
 
 /******** OWNER NUMBER**********/
-const ownerNumber = ["62895710073737@s.whatsapp.net","6282334297174@s.whatsapp.net"] 
+const ownerNumber = ["628156570323@s.whatsapp.net","628156570323@s.whatsapp.net"] 
 /************************************/
 
        
